52be263585f546ffa00bd8e9ee08257b102666ff
Rupesh <rupesh@Rupeshs-MacBook-Air.local>
2024-07-11 13:03:22 +0530

    js

---
+++ new.js
@@ -0,0 +1,39 @@
+
+// number
+// string
+// Boolean
+// undefined
+// null
+// BigInt
+// symbol
+// var a=false
+// console.log(a);
+// var let const
+// var a=5
+// var a=7
+// let b=10
+// b=10
+// const c=10
+//  c=193
+//  console.log(c);
+// let fistname
+// let firstName='srk'
+// let lastName='khan'
+// console.log(`my firstname is   ${firstName}  my lastname   ${lastName}`);
+// let first_name
+// console.log(firstName    +"        "+lastName);
+// console.log('srk\nkhan');
+// let a=false
+// console.log(a);
+// let a=5
+// let b=6
+// let a==5
+// = vs == vs === 
+// console.log(false==0);
+// let score=100
+// let to=score==100
+// console.log(to);
+// == vs === 
+// console.log(5==='5');
+
+
